87fdbf300007902f60ec099854373324fd3f9014
Rodrigo Sanabria <rodrigo@sanabria.dev>
2022-08-21 13:17:41 +0200

    chore: sign git tags

--- scripts/release.sh
+++ scripts/release.sh
@@ -16,6 +16,6 @@ yarn version --new-version $VERSION
 cd ..
 
 git commit -am "chore: release version $VERSION"
-git tag -a v$VERSION -m "chore: release version $VERSION"
+git tag -s -a v$VERSION -m "chore: release version $VERSION"
 git push
 git push origin v$VERSION
