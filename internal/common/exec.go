46f8ca5a6b6f12eee5c412ebba61b33f3aa968c3
Rodrigo Sanabria <rodrigo@sanabria.dev>
2022-08-21 11:38:22 +0200

    feat(flags): add interpolate to allow env sustitution to commands that not support it

--- internal/common/exec.go
+++ internal/common/exec.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"os/exec"
 
+	"github.com/cbroglie/mustache"
 	"github.com/urfave/cli/v2"
 )
 
@@ -16,6 +17,13 @@ func Exec(cCtx *cli.Context) error {
 	cmd := cCtx.Args().First()
 	cmdArgs := cCtx.Args().Tail()
 	override := cCtx.Bool("override")
+	interpolate := cCtx.Bool("interpolate")
+	if interpolate {
+		cmdArgs, err = Interpolate(cmdArgs, dict)
+		if err != nil {
+			return err
+		}
+	}
 
 	Load(dict, override)
 
@@ -24,4 +32,15 @@ func Exec(cCtx *cli.Context) error {
 	command.Stdout = os.Stdout
 	command.Stderr = os.Stderr
 	return command.Run()
+}
+
+func Interpolate(args []string, dict map[string]string) ([]string, error) {
+	for i, arg := range args {
+		interpolated, err := mustache.Render(arg, dict)
+		if err != nil {
+			return nil, err
+		}
+		args[i] = interpolated
+	}
+	return args, nil
 }
